65ccbb011b3a81a98289f519917c4b70420dee4d
JohnMutemi <jmtutorsalp@gmail.com>
2024-04-02 05:04:22 +0300

    commit mini project src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value;
+
+    if (taskDescription.trim() !== '') {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskDescription;
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'X';
+      deleteButton.addEventListener('click', ()=> {
+        taskItem.remove();
+      });
+
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+
+      taskInput.value = '';
+    }
+  });
 });
+
